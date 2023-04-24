cf86cbe11b54aa6e9c5d71b2f4c9dde4de03be13
Georgiy Iakunenko <goshanyakunenko@gmail.com>
2023-04-24 16:04:22 +0200

    add Sleep page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".menu");
 const body = document.querySelector("body");
 
 menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active');
+  menuBtn.classList.toggle("active");
   menu.classList.toggle('active');
   body.classList.toggle('active');
 });
@@ -39,15 +39,20 @@ const PopUp = document.querySelector(".pop-up");
 const PopUpBtn = document.querySelector(".pop-up__btn");
 
 function closeModal() {
-    PopUp.classList.remove("active");
-    PopUpInner.classList.remove("active");
-    body.classList.remove("active")
+  if (!menu.classList.contains("active")) {   
+    body.classList.remove("active");
+  } 
+
+  PopUp.classList.remove("active");
+  PopUpInner.classList.remove("active");
 }
 
 function openModal() {
-  PopUp.classList.add("active");
-  PopUpInner.classList.add("active");
-  body.classList.add("active");
+    PopUp.classList.add("active");
+    PopUpInner.classList.add("active");
+    body.classList.add("active");
+  
+  
 }
 
 PopUpBtn.addEventListener('click', () => {
@@ -64,4 +69,27 @@ PopUpInner.addEventListener("click", (e) => {
   if (!e.target.closest(".pop-up") ){
       closeModal();
     }
+});
+
+// form validation 
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const nameInput = document.querySelector('[name="name"]');
+  const genderInput = document.querySelector('[name="gender"]:checked');
+  const messageInput = document.querySelector('[name="message"]');
+  const agreeInput = document.querySelector('[name="agree"]');
+
+
+  if (nameInput.value.trim() === ""
+  || !genderInput || messageInput.value.trim() === "" || !agreeInput.checked) {
+
+    alert("please fill all form fields");
+
+  } else {
+    form.submit();
+    alert("Thank you, We will respons you as soon as posible");
+  }
 });
